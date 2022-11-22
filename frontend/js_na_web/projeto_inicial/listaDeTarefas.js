236d0e4d0d58740a7ec4e54311ca24949b38c095
TheSamuelVitor <thesamuelvitor@users.noreply.github.com>
2022-11-21 22:04:39 -0300

    start to do simple website

---
+++ frontend/js_na_web/projeto_inicial/listaDeTarefas.js
@@ -0,0 +1,12 @@
+const criarTarefa = (event) => {
+  event.preventDefault();
+  
+  const input = document.querySelector("[data-form-input]");
+  console.log(input.value);
+  
+  input.value = "";
+}
+
+const novaTarefa = document.querySelector("[data-form-button]");
+
+novaTarefa.addEventListener("click", criarTarefa);
