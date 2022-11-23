a8c1d87ad1dcc1b966444479f5d3119ec974b24c
TheSamuelVitor <thesamuelvitor@users.noreply.github.com>
2022-11-23 16:49:58 -0300

    feat add tasks on list

--- frontend/js_na_web/projeto_inicial/listaDeTarefas.js
+++ frontend/js_na_web/projeto_inicial/listaDeTarefas.js
@@ -1,11 +1,19 @@
 const criarTarefa = (event) => {
   event.preventDefault();
   
+  const lista = document.querySelector("[data-list]");
   const input = document.querySelector("[data-form-input]");
-  console.log(input.value);
   
+  const tarefa = document.createElement('li');
+  const conteudo = `<p class="content">${input.value}</p>`;
+  
+  tarefa.innerHTML = conteudo;
+  tarefa.classList.add('task')
+
+  lista.appendChild(tarefa);
+
   input.value = "";
-}
+};
 
 const novaTarefa = document.querySelector("[data-form-button]");
 
