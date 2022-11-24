02d154056a0da122875a0f66096dfa5d06ec17ab
TheSamuelVitor <thesamuelvitor@users.noreply.github.com>
2022-11-23 23:44:45 -0300

    feat: nao mostra as funcoes para o usuario

--- frontend/js_na_web/projeto_inicial/listaDeTarefas.js
+++ frontend/js_na_web/projeto_inicial/listaDeTarefas.js
@@ -1,20 +1,41 @@
-const criarTarefa = (event) => {
-  event.preventDefault();
-  
-  const lista = document.querySelector("[data-list]");
-  const input = document.querySelector("[data-form-input]");
-  
-  const tarefa = document.createElement('li');
-  const conteudo = `<p class="content">${input.value}</p>`;
-  
-  tarefa.innerHTML = conteudo;
-  tarefa.classList.add('task')
+(() => {
+  const criarTarefa = (event) => {
+    event.preventDefault();
 
-  lista.appendChild(tarefa);
+    const lista = document.querySelector("[data-list]");
+    const input = document.querySelector("[data-form-input]");
 
-  input.value = "";
-};
+    const tarefa = document.createElement('li');
+    const conteudo = `<p class="content">${input.value}</p>`;
 
-const novaTarefa = document.querySelector("[data-form-button]");
+    tarefa.innerHTML = conteudo;
+    tarefa.classList.add('task')
 
-novaTarefa.addEventListener("click", criarTarefa);
+    tarefa.appendChild(BotaoConclui())
+    lista.appendChild(tarefa);
+
+    input.value = "";
+  };
+
+  const novaTarefa = document.querySelector("[data-form-button]");
+
+  novaTarefa.addEventListener("click", criarTarefa);
+
+  const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button')
+    botaoConclui.classList.add("check-button")
+    botaoConclui.innerText = 'concluir'
+
+    botaoConclui.addEventListener("click", concluirTarefa);
+    return botaoConclui;
+  }
+
+  const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target
+
+    const tarefaCompleta = botaoConclui.parentElement
+
+    tarefaCompleta.classList.toggle('done')
+  }
+}
+)()
