8ece8f69a7e48131ba4a5dcc76fbf74120afc5bb
TheSamuelVitor <thesamuelvitor@users.noreply.github.com>
2022-11-30 15:25:37 -0300

    feat: usando fetch para fazer requisicoes

--- frontend/crud-js/2016-JS-na-web-crud-com-JavaScript-assincrono-projeto_inicial/projeto_inicial/admin/service/cliente-service.js
+++ frontend/crud-js/2016-JS-na-web-crud-com-JavaScript-assincrono-projeto_inicial/projeto_inicial/admin/service/cliente-service.js
@@ -2,32 +2,37 @@ const criaNovaLinha = (nome, email) => {
 
   const linhaNovaCliente = document.createElement('tr')
 
-  const conteudo =  `<td class="td" data-td>${nome}</td>
+  const conteudo = `<td class="td" data-td>${nome}</td>
   <td>${email}</td>
   <td>
-      <ul class="tabela__botoes-controle">
-          <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-          <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-      </ul>
+    <ul class="tabela__botoes-controle">
+      <li>
+        <a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a>
+      </li>
+      <li>
+        <button class="botao-simples botao-simples--excluir" type="button">Excluir</button>
+      </li>
+    </ul>
   </td>`
 
   linhaNovaCliente.innerHTML = conteudo
-
   return linhaNovaCliente
-
 }
 
 const tabela = document.querySelector(`[data-tabela]`)
-const http = new XMLHttpRequest();
 
-http.open('GET', 'http://localhost:3000/profile')
+const listaClientes = () => {
 
-http.send()
+  return fetch(`http://localhost:3000/profile`)
+    .then(resposta => {
+      return resposta.json()
+    })
 
-http.onload = () => {
-  const data = JSON.parse(http.response);
-  data.forEach(elemento => {
-    console.log(elemento.nome);
-    tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
-  });
 }
+
+listaClientes()
+  .then(data => {
+    data.forEach(elemento => {
+      tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
+    });
+  })
